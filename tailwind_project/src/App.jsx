e0b5a3d31ee707fe00339e1bf1a2d54f65a7180c
pratikShejwal <shejwalp147@gmail.com>
2025-03-18 21:28:54 +0530

    tailwind

--- tailwind_project/src/App.jsx
+++ tailwind_project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Card from './components/Card'
 import './App.css'
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
      <h1 class="text-black text-2xl bg-amber-200 rounded-2xl">Tailwind</h1>
+     <Card user="Pratik" text="My name is pratik"/>
+     <Card user="Nothing"/>
     </>
   )
 }
