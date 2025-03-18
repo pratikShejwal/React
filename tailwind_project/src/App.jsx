ce71aecb45f1e8cbf6222a60d563570edeeff8c5
pratikShejwal <shejwalp147@gmail.com>
2025-03-18 21:05:03 +0530

    tailwind_project

---
+++ tailwind_project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 class="text-black text-2xl bg-amber-200 rounded-2xl">Tailwind</h1>
+    </>
+  )
+}
+
+export default App
